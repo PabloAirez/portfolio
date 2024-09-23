ae1a47e99f2823a46df8f2cc372054efa4bfbee1
= <=>
2024-09-23 20:44:03 -0300

    Adicionando as redes sociais

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,8 @@
 import styles from "./banner.module.css";
 import perfil from "/perfil.png"
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 
 
 
@@ -16,6 +19,10 @@ const Banner = () => {
                 <div className={styles.flexCol}>
                   <h1 className={styles.title}>Olá! Eu me chamo <span className={`${styles.box} ${styles.boxPurple}`}>Pablo Santos</span></h1>
                   <h2 className={styles.subtitle}>Desenvolvedor Fullstack <span className={`${styles.box} ${styles.boxPurple}`}>PHP</span> e <span className={`${styles.box} ${styles.boxBlue}`}>React</span></h2>
+                  <div className={styles.flexRow}>
+                    <a href="https://github.com/PabloAirez" target="_blank"><FaGithub className={styles.icons}></FaGithub></a>
+                    <a href="https://www.linkedin.com/in/pablo-santos-712311223/" target="_blank"><FaLinkedin className={styles.icons}></FaLinkedin></a>
+                  </div>
                   <div className={styles.flexRow}>
                     <button className={styles.button}>Meus Projetos</button>
                     <button className={styles.button}>Mais sobre mim</button>
